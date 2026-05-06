8124df398871f5ae10aa60eb54b2935d8807df4c
Tiago Hinterholz <fh.tiago@gmail.com>
2026-05-06 11:21:34 -0300

    bidding: change name bid to bidding and create aggregate and event from place bid

---
+++ app/modules/bidding/domain/bidding_aggregate.py
@@ -0,0 +1,96 @@
+from datetime import datetime
+from typing import List
+import uuid
+
+from app.modules.bidding.domain.events.bid_events import BidPlacedEvent
+from app.modules.bidding.domain.exceptions.bidding_exceptions import InvalidBidPlaceException
+
+
+class Bidding:
+    def __init__(
+        self,
+        auction_id: uuid.UUID,
+        current_price: float,
+        minimun_increment: float, 
+        last_user_id: uuid.UUID | None, 
+        last_amount: float | None, 
+        timestamp: datetime | None = None,
+        id: uuid.UUID | None = None, 
+    ):
+        self._id = id or uuid.uuid4()
+        self._current_price = current_price
+        self._minimun_increment = minimun_increment
+        self._auction_id = auction_id
+        self._last_user_id = last_user_id
+        self._last_amount = last_amount
+        self._timestamp = timestamp or datetime.now()
+        self.events: List = []
+    
+    @classmethod
+    def open(cls, auction_id: uuid.UUID, starting_price: float, minimun_increment: float) -> "Bidding":
+        return cls(
+            auction_id=auction_id,
+            current_price=starting_price,
+            minimun_increment=minimun_increment,
+            last_user_id=None,
+            last_amount=None,
+        )
+    
+    @classmethod
+    def restore(cls, **kwargs) -> "Bidding":
+        bid = cls(**kwargs)
+        bid.events = []
+        return bid
+    
+    def place_bid(self, user_id: uuid.UUID, amount: float):
+        if amount < self._current_price + self._minimun_increment:
+            raise InvalidBidPlaceException(
+                f"Bid amount must be at least {self._current_price + self._minimun_increment}"
+            )
+        
+        self._last_user_id = user_id
+        self._last_amount = amount
+        self._current_price = amount
+        self._timestamp = datetime.now()
+        self.events.append(
+            BidPlacedEvent(
+                payload={
+                    "auction_id": str(self._auction_id),
+                    "user_id": str(user_id),
+                    "amount": amount
+                }
+        ))
+    
+    def pull_events(self) -> List:
+        events = self.events.copy()
+        self.events.clear()
+        return events
+    
+    @property
+    def id(self) -> uuid.UUID:
+        return self._id
+
+    @property
+    def current_price(self) -> float:
+        return self._current_price
+
+    @property   
+    def minimun_increment(self) -> float:
+        return self._minimun_increment
+
+    @property
+    def auction_id(self) -> uuid.UUID:
+        return self._auction_id
+
+    @property
+    def last_user_id(self) -> uuid.UUID | None:
+        return self._last_user_id
+
+    @property
+    def last_amount(self) -> float | None:
+        return self._last_amount
+
+    @property
+    def timestamp(self) -> datetime:
+        return self._timestamp
+    
---
+++ app/modules/bidding/domain/exceptions/bidding_exceptions.py
@@ -0,0 +1,6 @@
+from core.exceptions.exceptions import DomainException
+
+
+class InvalidBidPlaceException(DomainException):
+    def __init__(self, message: str):
+        super().__init__(message)
